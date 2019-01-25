b1c47d67433b6e86729c296bccbc1161a0edbbde
CollinsLiam21 <35492820+CollinsLiam21@users.noreply.github.com>
2019-01-24 20:11:10 -0500

    Updated from Brython Server: 1/24/2019 8:11:09 PM

--- ageRandomizer.py
+++ ageRandomizer.py
@@ -1,1 +1,19 @@
+L = [25,33,35,48,55,38,56,55,64,38]
 
+from random import randint
+
+a = randint(0,9) 
+num1 = L[a] 
+L2 = L 
+L2.remove(num1)
+
+b = randint(0,8) 
+num2 = L2[b] 
+L3 = L2 
+L3.remove(num2)
+
+c = randint(0,7) 
+num3 = L3[c]
+
+print(num1,num2,num3) 
+print('Average = ', (num1+num2+num3)/3)
